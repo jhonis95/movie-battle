cba27891905a70bed725d979851d76f2522ccb4c
jonatan de oliveira <j.jhonis95@gmail.com>
2022-05-27 16:25:38 -0400

    Understanding Debounce

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const myKey='7c85e71b'
-const fetchdata= async ()=>{
+const fetchdata= async (searchTerm)=>{
     const response = await axios.get('http://www.omdbapi.com/',{
         params:{
             apikey:myKey,
-            s:'avengers'
+            s:searchTerm
         }
     })
-    console.log(response)
-    console.log(response.data)
 }
-fetchdata()
+const debounce=(func)=>{
+    let timeoutId;
+    return (...args)=>{
+        console.log(args)
+        if(timeoutId){
+            clearInterval(timeoutId)//to end the setInterval
+        }
+        timeoutId=setInterval(()=>{//setInterval function return a id automatically
+            func.apply(null,args);
+        },1000)
+    }
+}
+const onInput=debounce((event)=>{
+    fetchdata(event.target.value)
+})
+const input =document.querySelector('input');
+input.addEventListener('input',onInput)
